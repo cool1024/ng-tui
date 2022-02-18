933e9f929389aec98b1a70e1dee5dbd4ab0a5618
cool1024 <xiaojiannangkang@126.com>
2022-02-18 15:30:17 +0800

    update angular version

---
+++ dev/projects/ng-tui/src/modules/upload/image-upload.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ts-image-upload',
+    templateUrl: 'image.html'
+})
+export class ImageUploadComponent {
+   
+}
---
+++ dev/projects/ng-tui/src/modules/upload/upload.moudle.ts
@@ -0,0 +1,7 @@
+import { NgModule, } from '@angular/core';
+import { TUICoreModule } from '../../tui-core/tui-core.module';
+@NgModule({
+    imports: [TUICoreModule],
+    declarations: [],
+})
+export class UploadModule { }
